8bb26de546f5198c58f931914b9798f0f5bab110
Maria Schmelzer-Ziringer <mariasz@gmail.com>
2021-04-22 23:36:45 +0200

    Base Version working

---
+++ hobby-blog-maria/actions/a_create.php
@@ -0,0 +1,52 @@
+<?php
+require_once 'db_connect.php';
+require_once 'file_upload.php';
+
+if ($_POST) {   
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $uploadError = '';
+    //this function exists in the service file upload.
+    $picture = file_upload($_FILES['picture']);  
+   
+    $sql = "INSERT INTO article (title, content, picture) VALUES ('$title', '$content', '$picture->fileName')";
+
+    if ($connect->query($sql) === true) {
+        $class = "success";
+        $message = "The entry below was successfully created <br>
+            <table class='table w-50'><tr>
+            <td> $title </td>
+            <td> $content </td>
+            </tr></table><hr>";
+        $uploadError = ($picture->error !=0)? $picture->ErrorMessage :'';
+    } else {
+        $class = "danger";
+        $message = "Error while creating record. Try again: <br>" . $connect->error;
+        $uploadError = ($picture->error !=0)? $picture->ErrorMessage :'';
+    }
+    $connect->close();
+} else {
+    header("location: ../error.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Update</title>
+        <?php require_once '../components/boot.php'?>
+    </head>
+    <body>
+        <div class="container">
+            <div class="mt-3 mb-3">
+                <h1>Create request response</h1>
+            </div>
+            <div class="alert alert-<?=$class;?>" role="alert">
+                <p><?php echo ($message) ?? ''; ?></p>
+                <p><?php echo ($uploadError) ?? ''; ?></p>
+                <a href='../index.php'><button class="btn btn-primary" type='button'>Home</button></a>
+            </div>
+        </div>
+    </body>
+</html>
